986dadd2bb9bc420e2923cb60cd0f04e195790bc
Kwapong Elvis <ekmustard16@gmail.com>
2026-06-11 02:41:55 +0000

    first commit

---
+++ src/Assignments/MyInfo.java
@@ -0,0 +1,19 @@
+package Assignments;
+
+public class MyInfo {
+    static void main() {
+
+        String fullName = "Kwapong Elvis Atiemo";
+        int age = 22;
+        char gender = 'M';
+        double height = 1.75;
+        float weight = 70.5f;
+        boolean isStudent = true;
+        long phoneNumber = 233552565352L;
+        String Level = "Level 200";
+        String course = " Computer Network Management";
+
+        System.out.println("My name is: " +fullName+ "I am:" +age+ "I am a Male: " +gender+ "My height is: " +height+ "My weight: " +weight+ "I am a student: " +isStudent+ "My phone number is: " +phoneNumber+ "I'm in Level: " +Level+ "I study: " +course);
+    }
+
+}
