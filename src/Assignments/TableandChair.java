59a88eb2ef32493ea7ab9f5f9c875cf9aa818d08
Kwapong Elvis <ekmustard16@gmail.com>
2026-06-11 03:13:52 +0000

    assignment

---
+++ src/Assignments/TableandChair.java
@@ -0,0 +1,13 @@
+package Assignments;
+
+public class TableandChair {
+    public void main(String[] args){
+
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("X       XXXXXXX         X");
+        System.out.println("XXXXXXX X      X  XXXXXXX");
+        System.out.println("X     X X      X  X     X");
+        System.out.println("X     X X      X  X     X");
+    }
+}
